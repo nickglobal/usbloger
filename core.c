b6c443ebbe764495d05d1cd117a2ca02831c4bfa
Nick <kol_slus@ukr.net>
2011-10-07 16:15:38 +0300

    fix comilation error

--- core.c
+++ core.c
@@ -2,14 +2,14 @@
 
 int main()
 {
-	int i,j;
+	int i,jj;
 	printf("usb log core \n");
 	printf("start count \n");
-	j = 0;
+	jj = 0;
 	for ( i=0; i<10; i++)
 	{
 		printf(" count %d \n", i);
-		if ( (j++) == 5) {
+		if ( (jj++) == 5) {
 		   printf("JJ\n");
 		   jj = 123;
 		} 	
