3147620718b148c1d9efbe8d792e15c0f0c3c005
Nick <kol_slus@ukr.net>
2011-10-07 14:49:12 +0300

    some changes in JJ functionality

--- core.c
+++ core.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-	int i;
+	int i,j;
 	printf("usb log core \n");
 	printf("start count");
+	j = 0;
 	for ( i=0; i<10; i++)
 	{
 		printf(" count %d", i);
+		if ( (j++) == 5) printf("JJ\n");
 	}
 	printf("count end\n");
 	return 0;
