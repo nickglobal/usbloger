5ae2410556f381fad95b637910c574abdd8037b9
Nick <kol_slus@ukr.net>
2011-10-07 15:04:23 +0300

    fix printout

--- core.c
+++ core.c
@@ -4,11 +4,11 @@ int main()
 {
 	int i,j;
 	printf("usb log core \n");
-	printf("start count");
+	printf("start count \n");
 	j = 0;
 	for ( i=0; i<10; i++)
 	{
-		printf(" count %d", i);
+		printf(" count %d \n", i);
 		if ( (j++) == 5) printf("JJ\n");
 	}
 	printf("count end\n");
